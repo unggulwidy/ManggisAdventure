1b56ab18538bef71173beeae8eba3c74366bf536
Unggul Widyanarko <49548061+unggulwidy@users.noreply.github.com>
2021-03-31 12:14:22 +0700

    Add files via upload

---
+++ data/TitikLokasi_8.js
@@ -0,0 +1,11 @@
+var json_TitikLokasi_8 = {
+"type": "FeatureCollection",
+"name": "TitikLokasi_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Poin": "Taman Senggani", "Dusun": "Petung", "Keterangan": "Berangkat dari sini" }, "geometry": { "type": "Point", "coordinates": [ 111.476243462515683, -8.200794420919198 ] } },
+{ "type": "Feature", "properties": { "Poin": "Rumah Pak RT", "Dusun": "Pagerwatu", "Keterangan": "Sebagai basecamp untuk pendakian kentheng" }, "geometry": { "type": "Point", "coordinates": [ 111.492255365011459, -8.201283405071617 ] } },
+{ "type": "Feature", "properties": { "Poin": "Puncak Kentheng", "Dusun": "Pagerwatu", "Keterangan": "Objek utama" }, "geometry": { "type": "Point", "coordinates": [ 111.491982930983653, -8.199401980332782 ] } },
+{ "type": "Feature", "properties": { "Poin": "Pondokan Krajan", "Dusun": "Krajan", "Keterangan": "Tempat istirahat mahasiswa KKN" }, "geometry": { "type": "Point", "coordinates": [ 111.482468696475152, -8.205660977483758 ] } }
+]
+}
